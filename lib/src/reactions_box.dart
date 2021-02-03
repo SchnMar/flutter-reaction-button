62715d3087d093b0a8bb8355779ca68445994135
Marcel <marcel.schneider.benningen@gmail.com>
2021-02-03 20:56:14 +0000

    a tiny bit further down

--- lib/src/reactions_box.dart
+++ lib/src/reactions_box.dart
@@ -151,7 +151,7 @@ class _ReactionsBoxState extends State<ReactionsBox>
                   : _getBottomPosition();
 
   double _getTopPosition() =>
-      widget.buttonOffset.dy - widget.buttonSize.height * 2.4;
+      widget.buttonOffset.dy - widget.buttonSize.height * 2.31;
 
   double _getBottomPosition() =>
       widget.buttonOffset.dy + widget.buttonSize.height;
